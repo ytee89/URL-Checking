12489837388bb03e801473ffc375b0789df0d566
azri89 <32289279+azri89@users.noreply.github.com>
2018-12-04 12:29:55 +0800

    Update urlaccess.py

--- packagetools/urlaccess.py
+++ packagetools/urlaccess.py
@@ -36,14 +36,14 @@ class URL(object):
         page = soup(html.content, "lxml")
         return page
     
-    def pdfmoddate(self, save_path):
+    def pdfmoddate(self, save_path, key):
         #read last modified date of pdf file    
         pdfFileObj = open(save_path, 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         
         try:
             last_update1 = pdfReader.getDocumentInfo()
-            last_update = str(last_update1['/ModDate'])
+            last_update = str(last_update1[key])
         except PyPDF2.utils.PyPdfError as pdferror:
             last_update = ""
             
@@ -65,11 +65,14 @@ class URL(object):
         page = json.loads(html.content.decode('utf-8'))
         return page
     
-    def dlfile(self, save_path):
+    def dlfile(self, save_path, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}):
         #download file from direct url using request
-        r = requests.get(self.url,verify=False, timeout=30)
-        with open(save_path,'wb') as f:   
-            f.write(r.content)
+        r = requests.get(self.url,verify=False, timeout=30, headers=headers, stream=True)
+        with open(save_path,'wb') as f:
+            for chunk in r.iter_content(1024):
+                if not chunk:
+                    break
+                f.write(chunk)
     
     def getdriver(self,dr):
         #open url using selenium
